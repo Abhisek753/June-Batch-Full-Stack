78d47e7538fcc4b9bdb6cfa64de6a38130f4056a
Abhisek753 <properachievement@gmail.com>
2026-08-10 22:36:32 +0530

    js class

---
+++ JAVASCRIPT/index.js
@@ -0,0 +1,77 @@
+"use strict"
+console.log("This is skillians first js class");
+console.log(6+4+8);
+console.log(6+4+8);
+let value=57;
+var fname="Karan";
+const age=34;
+console.log(value)
+console.log(fname);
+console.log(age)
+// console.log("hi skillians");
+
+// var firstname;
+// console.log(firstname);
+
+// let num=565657676767878787788787878787n
+// console.log(typeof num);
+
+// let id1=Symbol("id");
+
+// console.log(typeof id1);
+
+
+
+
+
+
+// {
+//   var greet="Hello My name is Rajan kumar"; 
+//   console.log("my log=> ",greet)
+// }
+
+// console.log("log no 2 =>",greet);
+
+// let age=30;
+//  age=50;
+// console.log(age);
+// let val="arjun";
+// console.log(val)
+
+// let address="Bangalore karnataka"
+
+// console.log(typeof (address));
+// let val=12.34;
+// console.log(typeof NaN);
+// console.log(typeof (2==2))
+
+// let myData={
+//     "name":"Raju",
+//     "email":"raju@gmail.com",
+//     "add":"Dholakpur",
+//     "age":5,
+//     "friend":"Jaggu"
+// }
+
+// console.log(myData["name"]);
+// let data=["monkey","dog","cat","cow","pig"];
+// console.log(data[2])
+
+// let value={
+//     "skills":["react","html","js","redux"],
+//     "batch":"skillians"
+// }
+// console.log(value["skills"][2])
+
+
+
+
+
+
+
+
+
+
+
+
+
