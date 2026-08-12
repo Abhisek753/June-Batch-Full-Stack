f13c1ebbf9a5763ca28976dd4d9b3e3762a52bd2
Abhisek753 <properachievement@gmail.com>
2026-08-12 22:09:39 +0530

    operators

---
+++ JAVASCRIPT/day2/index.js
@@ -0,0 +1,61 @@
+// let a=10;
+// let b=20;
+// let result=a+b;
+// console.log("sum + sub =>",result-2);
+// console.log("multiplication =>",a*b);
+// console.log(a/2);
+// console.log("exponential operator =>",2**4)
+// console.log("exponential operator =>",3**4)
+// console.log(10%2);
+// console.log(29%3);
+// Assignment operators
+// let x=5;
+// x+=4;
+// console.log(x);
+// x*=2;
+// console.log(x);
+
+
+// let a=40;
+// let b=50;
+// let result=a<b
+// console.log(result)
+// console.log(a>b);
+// console.log(a<b);
+// console.log(a==b);
+
+// let a1=40;
+// let b1="40";
+
+// console.log(a1==b1);
+// console.log(a1===b1);
+
+// let x=35;
+// let y="35";
+// console.log(x!==y)
+
+console.log(2<1&&4>2);
+
+console.log(2<1 || 4>2);
+
+console.log(2!=2)
+let value=100>50?"yes":"No";
+
+console.log(value);
+
+let x=201;
+let result=x%2==0?"Even Number":"Odd number";
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
