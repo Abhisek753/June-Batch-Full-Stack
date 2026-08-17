7c030ef6db40c1b595f8776ac27d254d9ead67c8
Abhisek753 <properachievement@gmail.com>
2026-08-17 22:35:15 +0530

    array method

---
+++ JAVASCRIPT/day3/index.js
@@ -0,0 +1,93 @@
+let array1=[1,3,5,7,9];
+// let array2=["apple","banana","orange","kiwi",8,9,12,true,false,true];
+// array2[2]="grapes";
+// console.log(array2);
+
+// console.log(array1.length)
+// console.log(array1[1]);
+// console.log(array2[8])
+// let userdata=[{name:"Abhisek",age:28,location:"Banglore"},{name:"Piyush",age:23,location:"Banaras"},{name:"Akshay",age:24,location:"Kolkata"}];
+
+// console.log(userdata[1].location);
+//adding
+// array1.push(12,55,100);
+// array1.unshift(20);
+
+let fruits=["apple","mango","banana","orange"];
+// let result=fruits.slice(1,3);
+// console.log(result)
+// fruits.splice(1,2,"lichi","papaya");
+// fruits.splice(2,2,"grapes")
+// console.log(fruits);
+
+// fruits.sort();
+
+// console.log(fruits);
+
+// let data=[3,5,2,9,4,8];
+// console.log(data.sort().reverse())
+
+
+
+
+
+//Remove
+// array1.pop()
+// console.log(array1);
+// array1.pop()
+// console.log(array1);
+// array1.shift();
+// console.log(array1)
+
+// let data=[2,3,4,5,6,7,8];
+
+// let ans=data.reverse();
+// let ans=data.includes(88)
+
+// console.log(data.indexOf(8))
+// console.log(data);
+// data.forEach((el,i)=>{
+//  let  val= el*2
+//     console.log(val);
+// });
+
+// let value=data.map((el,i)=>el*3);
+// console.log(data);
+// console.log(value);
+// console.log(data)
+
+// let ans=data.filter((el)=>el%2==0);
+// console.log(ans)
+// let res=data.find((el)=>el>4);
+// console.log(res);
+// let res=data.findIndex((el)=>el>6);
+// console.log(res);
+// let res=data.some((el)=>el>6);
+// let res=data.every((el)=>el>1);
+// console.log(res);
+
+// let passMarks=60;
+// let studentMarks=35;
+// if(studentMarks>passMarks){
+//     console.log("Passed");
+// }else{
+//     console.log("Failed");
+// }
+
+let marks=95;
+if(marks>70){
+    console.log("Star student");
+}else if(marks>=65){
+    console.log("good student");
+}else if(marks>50){
+      console.log("average student");
+}else{
+    console.log("week student");
+}
+
+
+
+
+
+
+
